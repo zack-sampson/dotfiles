f63c1aaa27d9e48777a92f48f963adef521a4b8c
Zack Sampson <zls@twosigma.com>
2017-12-14 18:29:12 +0000

    [zsh] prompt improvements

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -40,7 +40,7 @@ zstyle ':vcs_info:*' unstagedstr 'M'
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' actionformats '%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
 zstyle ':vcs_info:*' formats \
-  '%F{5}[%F{2}%b%F{5}] %F{2}%c%F{3}%u%f'
+  '%F{5}(%F{2}%b%F{5}) %F{2}%c%F{3}%u%f'
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
 zstyle ':vcs_info:*' enable git
 +vi-git-untracked() {
@@ -66,4 +66,4 @@ precmd () {
 }
 
 NEWLINE=$'\n'
-PROMPT='[%*] ${fdefault}[%n@${hostname_color}%m${fdefault}]:%3~ ${vcs_info_msg_0_} %f${NEWLINE} $ '
+PROMPT='[%*] ${fdefault}%n@${hostname_color}%m${fdefault}:%3~ ${vcs_info_msg_0_} %f»${NEWLINE} $ '
