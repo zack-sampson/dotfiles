643d024fd439779f6abe68316f717d5e4ae7a2b8
Zack Sampson <zls@twosigma.com>
2017-12-23 01:17:29 +0000

    [zsh] add 'history' alias

--- zsh/.zsh/history.zsh
+++ zsh/.zsh/history.zsh
@@ -7,6 +7,13 @@ fi
 HISTSIZE=10000
 SAVEHIST=10000
 
+case $HIST_STAMPS in
+  "mm/dd/yyyy") alias history='fc -fl 1' ;;
+  "dd.mm.yyyy") alias history='fc -El 1' ;;
+  "yyyy-mm-dd") alias history='fc =il 1' ;;
+  *) alias history='fc -l 1' ;;
+esac
+
 # share history across all sessions
 setopt SHARE_HISTORY
 
