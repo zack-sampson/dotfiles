4b0dccce1b254f291623e197ca515501876ca437
Zack Sampson <zls@twosigma.com>
2018-01-03 17:22:08 +0000

    [zsh] add ls aliases

--- zsh/.zsh/aliases.config.zsh
+++ zsh/.zsh/aliases.config.zsh
@@ -4,3 +4,11 @@ compinit
 compdef g='git'
 
 alias g=git
+
+# ls aliases
+alias ls='ls --color=auto'
+alias la='ls -A'
+alias ll='ls -l'
+alias llh='ls -lh'
+alias lla='ls -lA'
+alias llah='ls -lAh'
