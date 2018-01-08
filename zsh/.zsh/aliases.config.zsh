cec161d8fbcfc18a46c9a284eb04f428f0ce7cce
Zack Sampson <zls@twosigma.com>
2018-01-08 19:16:43 +0000

    [zsh][exa] set up ls to use exa if available

--- zsh/.zsh/aliases.config.zsh
+++ zsh/.zsh/aliases.config.zsh
@@ -6,9 +6,20 @@ compdef g='git'
 alias g=git
 
 # ls aliases
-alias ls='ls --color=auto'
-alias la='ls -A'
-alias ll='ls -l'
-alias llh='ls -lh'
-alias lla='ls -lA'
-alias llah='ls -lAh'
+if ! command -v exa >/dev/null ; then
+  alias ls='ls --color=auto'
+  alias la='ls -A'
+  alias ll='ls -l'
+  alias llh='ls -lh'
+  alias lla='ls -lA'
+  alias llah='ls -lAh'
+else
+  # let's try this out
+  alias ls='exa -la --git'
+  alias ll='exa -l'
+  alias la='exa -a'
+  alias lla='exa -la'
+  # exa is human readable by default
+  alias llah='exa -la'
+fi
+
