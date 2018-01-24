1fadeb415b07ad3ba2790006f78636bdeef44807
Zack Sampson <zls@twosigma.com>
2018-01-24 18:26:29 +0000

    [xmonad][xmobar] plug xmobar into xmonad

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -4,10 +4,16 @@ import XMonad.Config.Xfce
 import XMonad.Hooks.SetWMName
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
+import XMonad.Hooks.DynamicLog
 
 main = do
+  xmproc <- spawnPipe "xmobar >~/xmobar.out 2>~/xmobar.err"
   xmonad $ xfceConfig
     { startupHook = startupHook xfceConfig >> setWMName "LG3D"
+    , logHook = dynamicLogWithPP xmobarPP
+      { ppOutput = hPutStrLn xmproc
+      , ppTitle = xmobarColor "green" "" . shorten 50
+      }
     , terminal = "xfce4-terminal"
     , modMask = mod4Mask
     , borderWidth = 3
