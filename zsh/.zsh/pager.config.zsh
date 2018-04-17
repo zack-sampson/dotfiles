721819d676375003c37a31f9a019aa19eefe20bd
Zack Sampson <zls@twosigma.com>
2018-04-17 15:49:58 +0000

    [kak] set kak to pager

--- zsh/.zsh/pager.config.zsh
+++ zsh/.zsh/pager.config.zsh
@@ -1,4 +1,7 @@
-if command -v most > /dev/null 2>&1 ; then
-  export PAGER=most
+if command -v kak-pager > /dev/null 2>&1 ; then
+  export PAGER=kak-pager
   alias less=$PAGER
+fi
+if command -v kak-man-pager > /dev/null 2>&1 ; then
+  export MANPAGER=kak-man-pager
 fi
