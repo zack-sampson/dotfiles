3e7f0f3a5790a0cf696aedd95285abe97ac7a94a
Zack Sampson <zls@twosigma.com>
2018-05-01 16:43:46 +0000

    [xmonad] change bindings

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -5,6 +5,7 @@ import XMonad.Hooks.SetWMName
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
 import XMonad.Hooks.DynamicLog
+import System.Exit
 
 main = do
   xmproc <- spawnPipe "xmobar >~/xmobar.out 2>~/xmobar.err"
@@ -18,9 +19,30 @@ main = do
     , modMask = mod4Mask
     , borderWidth = 3
     }
+    -- unbind the silly defaults
+    -- these mouse unbinds don't seem to work
+    `removeMouseBindings`
+    [ (mod1Mask, button1)
+    , (mod1Mask, button2)
+    , (mod1Mask, button3)
+    ]
+    `removeKeysP`
+    [ "M-q"
+    , "M-S-q"
+    , "M-p"
+    , "M-S-p"
+    , "M-h"
+    , "M-l"
+    , "M-,"
+    , "M-."
+    , "M-n"
+    , "M-<Tab>"
+    , "M-S-<Space>"
+    ]
     `additionalKeysP`
     [ ("M-o", safeSpawnProg "google-chrome")
-    , ("M-r", spawn $ "dmenu_run" )
+    , ("M-d", spawn $ "dmenu_run" )
     , ("M-<Return>", spawn $ "xfce4-terminal --execute tmux")
+    , ("M-<Backspace>", spawn "xmonad --recompile; xmonad --restart")
     ]
 
