129a9ae834d128564544515692176f921064d50c
Zack Sampson <zls@twosigma.com>
2018-08-09 19:46:48 +0000

    [zsh] use functions instead of aliases

--- zsh/.zsh.d/aliases.config.zsh
+++ zsh/.zsh.d/aliases.config.zsh
@@ -3,24 +3,48 @@ compinit
 
 compdef g='git'
 
-alias g=git
+function g() {
+  git "$@"
+}
 
 # ls aliases
 if ! command -v exa >/dev/null ; then
-  alias ls='ls --color=auto'
-  alias la='ls -A'
-  alias ll='ls -l'
-  alias llh='ls -lh'
-  alias lla='ls -lA'
-  alias llah='ls -lAh'
+  function ls() {
+    ls --color=auto "$@"
+  }
+  function la() {
+    ls -A "$@"
+  }
+  function ll() {
+    ls -l "$@"
+  }
+  function llh() {
+    ls -lh "$@"
+  }
+  function lla() {
+    ls -lA "$@"
+  }
+  function llah() {
+    ls -lAh "$@"
+  }
 else
   # let's try this out
   # can't use --git because it's too slow with meta
-  alias ls='exa -la'
-  alias ll='exa -l'
-  alias la='exa -a'
-  alias lla='exa -la'
+  function ls() {
+    exa -la "$@"
+  }
+  function ll() {
+    exa -l "$@"
+  }
+  function la() {
+    exa -a "$@"
+  }
+  function lla() {
+    exa -la "$@"
+  }
   # exa is human readable by default
-  alias llah='exa -la'
+  function llah() {
+    exa -la "$@"
+  }
 fi
 
