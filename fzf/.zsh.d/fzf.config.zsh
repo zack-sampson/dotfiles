a99cbb90e652013d42cf47f91ed3ba6053ef70d0
Zack Sampson <zls@twosigma.com>
2018-08-14 20:03:32 +0000

    [fzf][zsh] add fzf keybindings

--- fzf/.zsh.d/fzf.config.zsh
+++ fzf/.zsh.d/fzf.config.zsh
@@ -1,3 +1,11 @@
+if [ -d $HOME/.nix-profile ] ; then
+    source $HOME/.nix-profile/share/fzf/key-bindings.zsh
+    source $HOME/.nix-profile/share/fzf/completion.zsh
+else
+    source /usr/share/fzf/key-bindings.zsh
+    source /usr/share/fzf/completion.zsh
+fi
+
 # stolen from https://mike.place/2017/fzf-fd/
 export FZF_DEFAULT_COMMAND="fd . $HOME"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
