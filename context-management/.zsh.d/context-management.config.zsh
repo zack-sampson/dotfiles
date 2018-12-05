67f79662faa9b5ae2cef72648fd9942717ebfe00
Zack Sampson <zls@twosigma.com>
2018-12-05 21:40:09 +0000

    [cm] add basic context tools

---
+++ context-management/.zsh.d/context-management.config.zsh
@@ -0,0 +1,9 @@
+
+if [ -f $HOME/bin/get-directory-for-context ]; then
+  function c {
+    local dir=$($HOME/bin/get-directory-for-context "$@")
+    if [ -n "${dir}" ] ; then
+      cd ${dir}
+    fi
+  } 
+fi
