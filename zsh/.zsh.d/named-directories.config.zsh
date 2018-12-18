d1b7e410f5b1ac50a13ce70d4ed643307652c904
Zack Sampson <zls@twosigma.com>
2018-12-18 17:55:22 +0000

    [zsh] remove named directories, use contexts

--- zsh/.zsh.d/named-directories.config.zsh
+++
@@ -1,6 +0,0 @@
-# set named directory aliases to match contexts
-# not sure how to do this dynamically if new contexts are created...
-/usr/bin/find $HOME/contexts -mindepth 1 -maxdepth 1 -type d |
-while read dir ; do
-  eval $(basename $dir)="${dir}"
-done
