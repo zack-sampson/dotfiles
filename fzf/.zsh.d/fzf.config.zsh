cd233c12ff642385f91ec219b6c5cff9da4c8caa
Zack Sampson <zls@twosigma.com>
2020-07-15 11:16:12 -0400

    [fzf] configure fzf correctly for osx
    Homebrew installs fzf's completions somewhere totally different from
    where Linux does, so we need to correctly branch if we're running OSX.

--- fzf/.zsh.d/fzf.config.zsh
+++ fzf/.zsh.d/fzf.config.zsh
@@ -3,11 +3,26 @@ if ! which fzf >/dev/null 2>&1 ; then
 fi
 
 if [ -d $HOME/.nix-profile ] ; then
-    source $HOME/.nix-profile/share/fzf/key-bindings.zsh
-    source $HOME/.nix-profile/share/fzf/completion.zsh
+  source $HOME/.nix-profile/share/fzf/key-bindings.zsh
+  source $HOME/.nix-profile/share/fzf/completion.zsh
+elif [ z"Mac" = z"${ZLS_OS}" ] ; then
+  # Copied from running the install script
+  # Setup fzf
+  # ---------
+  if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+    export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+  fi
+  
+  # Auto-completion
+  # ---------------
+  [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+  
+  # Key bindings
+  # ------------
+  source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 else
-    source /usr/share/fzf/key-bindings.zsh
-    source /usr/share/fzf/completion.zsh
+  source /usr/share/fzf/key-bindings.zsh
+  source /usr/share/fzf/completion.zsh
 fi
 
 # stolen from https://mike.place/2017/fzf-fd/
