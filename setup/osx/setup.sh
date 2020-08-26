8d2ee78a02872ea47c8ce49d3d328d57af5f8a8b
Zack Sampson <zls@Zacks-MBP.fios-router.home>
2020-08-26 11:44:29 -0400

    [setup] install homebrew

--- setup/osx/setup.sh
+++ setup/osx/setup.sh
@@ -4,3 +4,8 @@ set -exuo pipefail
 
 # Create this as a real directory so stow doesn't symlink it
 mkdir -p ${HOME}/.config
+
+# Install Homebrew
+if ! which brew ; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
