337ecbace6534957feef29f57395238c5c91a707
Zack Sampson <zls@twosigma.com>
2020-11-22 20:08:03 -0800

    [zsh][git] attempt to disable remote branch comp
    I'm trying to make my `git` tab completion much faster. I think all
    these remote calls are slowing it down. This change didn't quite speed
    things up, but it's a step in the right direction.

--- zsh/.zsh.d/compdef.config.zsh
+++ zsh/.zsh.d/compdef.config.zsh
@@ -6,3 +6,7 @@ fpath[1,0]=$HOME/.zsh.d/functions
 
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path $HOME/.zsh/cache
+
+# disable completion of remote git branches"
+zstyle ':completion::complete:git-checkout:argument-rest:headrefs' command "git for-each-ref --format='%(refname)' refs/heads 2>/dev/null"
+
