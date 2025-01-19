864eb9535f547a0e1d5d9c5759495b25252ac9ed
Zack Sampson <zls@fastmail.com>
2025-01-19 16:12:00 -0500

    Use PragmataPro in WezTerm
    Such a nice font! Let's use it!
    
     #wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,13 +1,10 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+config.font = wezterm.font('PragmataPro Mono Liga')
+config.font_size = 18
 
--- For example, changing the color scheme:
 config.color_scheme = 'Gruvbox (Gogh)'
 
--- and finally, return the configuration to wezterm
 return config
